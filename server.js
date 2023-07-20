89ca79f6ce9676ff348ef7b17ee616e446682533
Svetlana Panenko <svetabagira1986@gmail.com>
2023-07-20 15:36:19 +0300

    init project

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require("./app");
+
+const DB_HOST =
+  "mongodb+srv://Svitlana:oxpndBKTqDWlaKKx@cluster0.dhx9tv8.mongodb.net/db-cjntacts?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB_HOST)
+  .then(
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    })
+  )
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
